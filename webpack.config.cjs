9b486aade43b7a5a666ebbc2a8413faaace356a8
Mohsin.L <Mohsin.L@allshoretalent.com>
2025-03-28 02:55:44 +0500

    minor fixes

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "", // ✅ Change from "/" to ""
+    publicPath: "/gm_middleware_frontend/", // ✅ Change from "/" to ""
   },
 
   mode: "development",
